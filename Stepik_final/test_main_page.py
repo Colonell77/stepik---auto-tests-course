2ab13ca085d8eb4dc5c32e6722c2691118088e7c
Colonell77 <colonell@mail.ru>
2020-07-16 09:24:55 +0500

    Revert "Добавлена проверка страницы логина и регистрации"
    This reverts commit d19fd117

--- Stepik_final/test_main_page.py
+++ Stepik_final/test_main_page.py
@@ -1,5 +1,4 @@
 from .Pages.main_page import MainPage
-from .Pages.login_page import LoginPage
 
 
 def test_guest_can_go_to_login_page(browser):
@@ -8,5 +7,3 @@ def test_guest_can_go_to_login_page(browser):
     page.open()                      # открываем страницу
     page.go_to_login_page()          # выполняем метод страницы - переходим на страницу логина
     page.should_be_login_link()
-    log_page = LoginPage(browser, link)
-    log_page.should_be_login_page()
